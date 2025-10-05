eca6d2b6859bc28664526862b73f084c1abfaad9
FiveN1 <tomas.verheyen222@gmail.com>
2025-10-05 10:41:17 +0200

    proto web 2
    stále hledám nějaký styl který by se ke mě hodil...
    už se tu začínají rýsovat základy

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+console.log("Hello World!");
+
+setStyle();
+
+function setStyle() {
+  console.log("set style");
+
+  //document.getElementsByTagName('card_postcard_image_test')[0].style.backgroundColor = 'green';
+}
+
+
+/*
+main();
+
+//
+// start here
+//
+
+
+function main() {
+  const canvas = document.querySelector("#gl_canvas");
+
+  // Initialize the GL context
+  const gl = canvas.getContext("webgl");
+
+  // Only continue if WebGL is available and working
+  if (gl === null) {
+    alert(
+      "Unable to initialize WebGL. Your browser or machine may not support it.",
+    );
+    return;
+  }
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.5, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+*/
+
+// webgl
+// https://stackoverflow.com/questions/71499942/how-do-i-apply-a-2d-shader-to-a-webgl-canvas-in-js
